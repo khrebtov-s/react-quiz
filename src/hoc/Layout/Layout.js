27a8f7979f52b705d67199efffa3c2417e595550
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2020-03-12 14:38:38 +0700

    added menu button

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import MenuToggle from '../../components/Navigation/Menu Toggle/MenuToggle'
 import style from './layout.css';
 
 class Layout extends Component {
+    state = {
+        menu: false
+    }
+
+    toggleManuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
     render() {
         return (
             <div className={style.Layout}>
+                <MenuToggle
+                    onToggle={this.toggleManuHandler}
+                    isOpen={this.state.menu}
+                />
                 <main>
                     {this.props.children}
                 </main>
