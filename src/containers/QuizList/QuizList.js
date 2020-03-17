02e92218329bb355673ef9186368e80e9dc11191
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2020-03-17 16:04:04 +0700

    create def pages

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class QuizList extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
