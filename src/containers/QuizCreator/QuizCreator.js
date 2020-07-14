7f9b336a27f195181f35c53ae48c3d07d1462ca7
Denisenko Denis <sergey.khrebtov@sibers.com>
2020-07-14 19:40:31 +0700

    create add questions

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,14 +1,20 @@
 import React from "react";
 import classes from "./QuizCreator.css";
 import Button from "../../components/UI/button/button";
-import { createControl } from "../../form/formFramework";
 import Input from "../../components/UI/Input/Input";
+import Select from "../../components/UI/Select/Select";
+import {
+  createControl,
+  validate,
+  validateForm,
+} from "../../form/formFramework";
 
 function createOptionControl(num) {
   return createControl(
     {
       label: `Option ${num}`,
       errorMessage: "Value cannot be empty",
+      id: num,
     },
     { required: true }
   );
@@ -33,30 +39,79 @@ function createFormControls() {
 export default class QuizCreator extends React.Component {
   state = {
     quiz: [],
-    formControls: {
-      question: createControl(
-        {
-          label: "Enter a question",
-          errorMessage: "Question cannot be empty",
-        },
-        { required: true }
-      ),
-      option1: createOptionControl(1),
-      option2: createOptionControl(2),
-      option3: createOptionControl(3),
-      option4: createOptionControl(4),
-    },
+    rightAnswerId: 1,
+    isFormValid: false,
+    formControls: createFormControls(),
   };
 
   submitHandler = (e) => {
     e.preventDefault();
   };
 
-  addQuestionHandler = () => {};
+  addQuestionHandler = (e) => {
+    e.preventDefault();
+    const quiz = this.state.quiz.concat();
+    const index = quiz.length + 1;
 
-  createQuizHandler = () => {};
+    const {
+      question,
+      option1,
+      option2,
+      option3,
+      option4,
+    } = this.state.formControls;
 
-  changeHandler = (value, controlName) => {};
+    const questionItem = {
+      question: question.value,
+      id: index,
+      rightAnswerId: this.state.rightAnswerId,
+      answers: [
+        {
+          text: option1,
+          id: option1.id,
+        },
+        {
+          text: option2,
+          id: option2.id,
+        },
+        {
+          text: option3,
+          id: option3.id,
+        },
+        {
+          text: option4,
+          id: option4.id,
+        },
+      ],
+    };
+
+    quiz.push(questionItem);
+
+    this.setState({
+      quiz,
+      rightAnswerId: 1,
+      isFormValid: false,
+      formControls: createFormControls(),
+    });
+  };
+
+  createQuizHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state.quiz);
+    // TODO: server
+  };
+
+  changeHandler = (value, controlName) => {
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+
+    control.touched = true;
+    control.value = value;
+    control.valid = validate(control.value, control.validation);
+
+    formControls[controlName] = control;
+    this.setState({ formControls, isFormValid: validateForm(formControls) });
+  };
 
   renderInputsControls() {
     return Object.keys(this.state.formControls).map((controlName, index) => {
@@ -80,14 +135,31 @@ export default class QuizCreator extends React.Component {
     });
   }
 
+  selectChangeHandler = (e) => {
+    this.setState({ rightAnswerId: +e.target.value });
+  };
+
   render() {
+    const select = (
+      <Select
+        label="Choose the correct answer"
+        value={this.state.rightAnswerId}
+        onChange={this.selectChangeHandler}
+        options={[
+          { text: 1, value: 1 },
+          { text: 2, value: 2 },
+          { text: 3, value: 3 },
+          { text: 4, value: 4 },
+        ]}
+      />
+    );
     return (
       <div className={classes.QuizCreator}>
         <div>
           <h1>Quiz Creator</h1>
           <form onSubmit={this.submitHandler}>
             {this.renderInputsControls()}
-            <select name="" id=""></select>
+            {select}
             <Button type="primary" onClick={this.addQuestionHandler}>
               Add question
             </Button>
